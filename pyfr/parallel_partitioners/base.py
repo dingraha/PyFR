cc017d53e1d449304ecfa708c6581d7aceaed8ef
Ingraham, Daniel James (GRC-LTV0) <daniel.j.ingraham@nasa.gov>
2016-09-08 16:15:36 -0400

    Distributing the con_p0 array

--- pyfr/parallel_partitioners/base.py
+++ pyfr/parallel_partitioners/base.py
@@ -11,7 +11,7 @@ from pyfr.mpiutil import get_comm_rank_root
 Graph = namedtuple('Graph', ['vtab', 'etab', 'vwts', 'ewts'])
 
 
-def decomp_idx(Ntotal, Nsections):
+def split(Ntotal, Nsections):
     # Return indices that would split a thing of length l into n parts.
     # Shamelessly stolen from NumPy's array_split function:
     # http://docs.scipy.org/doc/numpy/reference/generated/numpy.array_split.html
@@ -30,45 +30,98 @@ class BaseParallelPartitioner(object):
     def __init__(self):
         # Get the number of MPI ranks.
         comm, rank, root = get_comm_rank_root()
+
         self.nparts = comm.size
+
+    def _read_partial_con(self, mesh):
+        # Get the number of MPI ranks.
+        comm, rank, root = get_comm_rank_root()
+
+        # Number of face connections.
+        nf = mesh['con_p0'].shape[1]
+        if rank == 0:
+            print("nf = {}".format(nf))
+
+        # The min and max connections this MPI process will read in.
+        div_points = split(nf, self.nparts)[rank:rank+2]
+
+        # Read the connectivity.
+        con = mesh['con_p0'][:,slice(*div_points)].astype('U4,i4,i1,i1')
+
+        return con
 
 
     def _distribute_con(self, mesh):
+        nparts = self.nparts
+
+        # Handy MPI stuff.
+        comm, rank, root = get_comm_rank_root()
 
         # Read in a subset of the ``con_p0`` dataset.
-        nf = mesh['con_p0'].shape[1]
-        div_points = decomp_idx(nf, self.nparts)[rank:rank+2]
-        con = mesh['con_p0'][:,slice(*div_points)].astype('U4,i4,i1,i1')
+        con = self._read_partial_con(mesh)
 
-        # Decide which elements will be ours.
-        el_d = {}
-        for tel, nel in mesh.partition_info('spt').items():
-            el_d[tel] = decomp_idx(nel[0], self.nparts)
+        # Duplicate the connections (b -> a for each a -> b).
+        con = np.hstack([con, con[::-1]])
 
-        # Build up a map of interfaces.
-        con_d = defaultdict(list)
-        for el, er in zip(*con):
-            lid = (el[0], el[1])
-            rid = (er[0], er[1])
-            con_d[lid].append(rid)
-            con_d[rid].append(lid)
+        # Decide which elements belong to each MPI rank (Element Type to
+        # Element Range MAP).
+        etermap = {et: split(en[0], nparts) for et, en in mesh.partition_info('spt').items()}
 
-        con_d_part = defaultdict(list)
-        for base_rank in range(self.nparts):
-            for tel in sorted(el_d.keys()):
-                emin, emax = el_d[tel][base_rank], el_d[tel][base_rank+1]
-                el_to_send = {(et, ei): elems for (et, ei), elems in con_d.items() if ei >= emin and ei < emax and et == tel}
+        if rank == 0:
+            print("etermap = {}".format(etermap))
 
-                ret = comm.gather(el_to_send, root=base_rank)
-                if rank == base_rank:
-                    for cons in ret:
-                        for node, edges in cons.items():
-                            con_d_part[node] += edges
+        # So, now each MPI process will distribute the interface
+        # information. 
+        for brank in range(nparts):
+            # Need to sort the keys, since we need each MPI process to
+            # call MPI_GATHER in the same order. Unless there's a
+            # non-blocking version. Is there? Yes, but only in MPI3, I
+            # think. And mpi4py doesn't appear to have it.
+
+            # Send all the connections that MPI rank `brank` will own.
+            for et in sorted(etermap.keys()):
+                # Min and max element IDs that `brank` will own.
+                emin, emax = etermap[et][brank], etermap[et][brank+1]
+
+                # Extract the relevent interfaces from con.
+                idx = np.all(
+                        [
+                            con['f0'][0] == et,
+                            con['f1'][0] >= emin,
+                            con['f1'][0] < emax
+                        ], axis=0
+                    )
+                consend = con[:, idx]
+
+                # Send the connections.
+                conrecv = comm.gather(consend, root=brank)
+                if rank == brank:
+                    conrecv = np.hstack(conrecv)
+                    print('conrecv(rank={}) =\n{}'.format(rank, conrecv))
+                
+
+#       # Build up a map of interfaces.
+#       con_d = defaultdict(list)
+#       for el, er in zip(*con):
+#           lid = (el[0], el[1])
+#           rid = (er[0], er[1])
+#           con_d[lid].append(rid)
+#           con_d[rid].append(lid)
+
+#       con_d_part = defaultdict(list)
+#       for base_rank in range(self.nparts):
+#           for tel in sorted(el_d.keys()):
+#               emin, emax = el_d[tel][base_rank], el_d[tel][base_rank+1]
+#               el_to_send = {(et, ei): elems for (et, ei), elems in con_d.items() if ei >= emin and ei < emax and et == tel}
+
+#               ret = comm.gather(el_to_send, root=base_rank)
+#               if rank == base_rank:
+#                   for cons in ret:
+#                       for node, edges in cons.items():
+#                           con_d_part[node] += edges
 
 
-        print("rank = {}, con_d_part = {}".format(rank, con_d_part))
-
-
+#       print("rank = {}, con_d_part = {}".format(rank, con_d_part))
 
 
 
@@ -80,7 +133,7 @@ class BaseParallelPartitioner(object):
 
         # Figure out which part of the mesh is ours.
         nf = mesh['con_p0'].shape[1]
-        div_points = decomp_idx(nf, self.nparts)[rank:rank+2]
+        div_points = split(nf, self.nparts)[rank:rank+2]
 
         # Edges of the dual graph
         con = mesh['con_p0'][:,slice(*div_points)].astype('U4,i4,i1,i1')
@@ -135,7 +188,7 @@ class BaseParallelPartitioner(object):
 
             # Read in a subset of the ``con_p0`` dataset.
             #nf = mesh['con_p0'].shape[1]
-            #div_points = decomp_idx(nf, self.nparts)[rank:rank+2]
+            #div_points = split(nf, self.nparts)[rank:rank+2]
             #con = mesh['con_p0'][:,slice(*div_points)].astype('U4,i4,i1,i1')
 
             # Start building up the graph.
@@ -151,7 +204,7 @@ class BaseParallelPartitioner(object):
             ## ``spt_<element type>_p0``. So
             #el_d = {}
             #for tel, nel in mesh.partition_info('spt').items():
-            #    el_d[tel] = decomp_idx(nel[0], self.nparts)
+            #    el_d[tel] = split(nel[0], self.nparts)
 
             #con_d_part = defaultdict(list)
             #for base_rank in range(self.nparts):
@@ -167,13 +220,18 @@ class BaseParallelPartitioner(object):
 
             #print("rank = {}, con_d_part = {}".format(rank, con_d_part))
 
-            pgraph, vetimap = self._construct_partial_graph(mesh, rank)
-            print("rank = {}, pgraph = {}, vetimap = {}".format(rank,
-                pgraph, vetimap))
+            #pgraph, vetimap = self._construct_partial_graph(mesh, rank)
+            #print("rank = {}, pgraph = {}, vetimap = {}".format(rank,
+            #    pgraph, vetimap))
+
+            self._distribute_con(mesh)
+
+            # Dummy for now.
+            newmesh = mesh
 
         # Short circuit
-        else:
-            newmesh = mesh
+        #else:
+        #    newmesh = mesh
 
         # This doesn't work right now, I think because I'm using a
         # read-only .pyfrm file, and I'm not copying the old mesh object
